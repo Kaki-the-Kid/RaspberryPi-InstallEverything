016f6eeb708e5e7ef81e64b8fe8f38bc5a96b143
Karsten Reitan Sørensen <44589560+Kaki-the-Kid@users.noreply.github.com>
2022-12-19 12:39:11 +0100

    Update LAMPscript.sh

--- LAMPscript.sh
+++ LAMPscript.sh
@@ -169,4 +169,144 @@ sudo rm /var/www/html/latest.tar.gz
 
 echo "Ready, go to http://'your ec2 url'/blog and enter the blog info to finish the installation."
 
+#********************************************
+# PHPMyAdmin
+#********************************************
+# Install phpmyadmin
+# Normally one would use following command
+# sudo apt install phpmyadmin
+# But that gives you a older version
+# We want the latest
+
+# Change dir and download latest version
+cd ~/Downloads/
+wget -O phpmyadmin.tar.gz https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
+
+# Create a new directory where phpMyAdmin will be stored and extract tar.gz file:
+sudo mkdir /usr/share/phpmyadmin
+sudo tar xf phpmyadmin.tar.gz --strip-components=1 -C /usr/share/phpmyadmin
+
+# The tar.gz file is no longer required, we can remove it:
+rm -rf phpmyadmin.tar.gz
+
+# Create directory for phpMyAdmin temporary files and make www-data user as owner:
+sudo mkdir -p /var/lib/phpmyadmin/tmp
+sudo chown -R www-data:www-data /var/lib/phpmyadmin
+
+# The phpMyAdmin provides a sample configuration file. Create a copy of this file named config.inc.php:
+sudo cp /usr/share/phpmyadmin/config.sample.inc.php /usr/share/phpmyadmin/config.inc.php
+
+# Using text editor open a configuration file:
+echo "Replace the line: $cfg['blowfish_secret'] = '';"
+echo "with: $cfg['blowfish_secret'] = 'RV%Hg)2_Bt_)[%mAC24;#$Wu+)3du9}q';"
+sudo nano /usr/share/phpmyadmin/config.inc.php
+
+# This secret passphrase will be used for encryption. Also we need to specify 
+# temporary directory where phpMyAdmin can store cache. Add the following line 
+# to the end of a file:
+echo "Puth this at the end of the file"
+$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';
+
+# Now we need to configure Apache. Create Apache configuration file for 
+# phpMyAdmin:
+sudo touch /etc/apache2/conf-available/phpmyadmin.conf
+sudo echo "Alias /phpmyadmin /usr/share/phpmyadmin
+
+<Directory /usr/share/phpmyadmin>
+    Options Indexes FollowSymLinks
+    AllowOverride None
+    Require all granted
+</Directory>" > /etc/apache2/conf-available/phpmyadmin.conf
+
+# Enable the phpMyAdmin site:
+sudo a2enconf phpmyadmin.conf
+
+# Run the following command to restart Apache service:
+sudo service apache2 restart
+
+
+#********************************************
+# Install FTP Server on Raspberry Pi
+#********************************************
+# To install FTP server, run below command
+sudo apt install -Y pure-ftpd
+
+# Once the FTP server is installed, you can try to connect to FTP server 
+# using FileZilla software
+# Use the Raspberry Pi IP Address and credentials to connect
+# In my case the IP Address is 192.168.1.5 and the login credentials is 
+# pi as username and raspberry as password
+# No need to type anything for port, because we are using the default port, 
+# which is 21
+# To upload our web page or web application to raspberry pi we need to have 
+# root access to that folder. Run below command
+sudo -i
+# Now move to that html folder 
+cd /var/www/html/
+
+# Create new folder mkdir sa. I want to upload my web app to folder sa
+# go back to root folder. Run command 
+cd ..
+
+# Provide enough access for FTP to this folder 
+chmod 777 /var/www/html/sa/
+
+# Restart FTP server. 
+sudo service pure-ftpd restart
+
+# Our FTP account should have enough access to upload files to this folder now
+# Go to FileZilla and reconnect to Raspberry Pi.
+# Once we are connected again to Raspberry Pi find your folder on both raspberry 
+# pi and local computer
+# Upload your web page files to raspberry pi
+# I am going to upload my home automation web application code to raspberry pi 
+# now.
+#I have published this in Github here
+
+
+#********************************************
+# Install Samba Server on Raspberry Pi
+#********************************************
+# Run command 
+sudo apt install -y samba samba-common-bin
+
+# Type Y and hit enter key when you find any confirmation message
+# Now create a folder which should be set as shared folder. For that 
+# run below command
+mkdir /home/pi/shared
+
+# Next we need to provide full access to this fodler.
+# Run below command and edit the smb configuration file to provide full access
+
+
+# Now you can see the file open in cmd.
+echo "Go to the end of that file and then paste below text"
+echo "[Public SDCard]
+Comment = Public Folder from SDCard
+Path = /home/pi/shared
+Browseable = yes
+Writeable = Yes
+only guest = no
+create mask = 0644
+directory mask = 0755
+force create mask = 0644
+force directory mask = 0755
+force user = root
+force group = root
+Public = yes
+Guest ok = yes
+read only = no"
+
+read "ok"
+
+sudo nano /etc/samba/smb.conf
+
+# Finally we need to create a user for Samba. Run below command
+sudo smbpasswd -a pi
+
+# Finally, before we connect to our Raspberry Pi Samba share, we need to 
+# restart the samba service so that it loads in our configuration changes
+sudo systemctl restart smbd
+
 fi
+
